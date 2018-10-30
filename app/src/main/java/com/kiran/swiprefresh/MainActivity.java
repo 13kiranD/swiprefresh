9c5f9d8c8a913c2f20c1cc7651971ea692e174b1
13kiranD <kiranprdu1311@gmail.com>
2018-10-30 12:12:18 +0530

    Initial commit

---
+++ app/src/main/java/com/kiran/swiprefresh/MainActivity.java
@@ -0,0 +1,20 @@
+package com.kiran.swiprefresh;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.mySwipe);
+        TextView textView= (TextView) findViewById(R.id.myTextView);
+        SwipeRefreshLayout.setOnRefreshListner (new SwipeRefreshLayout.onRefreshListner())
+
+    }
+}
